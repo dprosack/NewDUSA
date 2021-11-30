02f62f826ebd29d85a7f981e022f244efe25f7f2
dprosack <59172965+dprosack@users.noreply.github.com>
2021-11-30 10:49:34 -0600

    update
    add modify line logic

---
+++ main.js
@@ -0,0 +1,88 @@
+
+let mainFunc = () => {
+    require([ "esri/Map",
+      "esri/views/MapView",
+      "esri/widgets/Sketch",
+      "esri/layers/GraphicsLayer",
+      "vue",
+      "esri/geometry/geometryEngine"], (
+        Map,
+        MapView,
+        Sketch,
+        GraphicsLayer,
+        Vue,
+        geometryEngine
+      ) => {
+        const gLayer = new GraphicsLayer();
+        const map = new Map({
+            basemap: "topo-vector",
+            layers: [gLayer]
+        });
+  
+        const mapView = new MapView({
+          container: "viewDiv",
+          map: map,
+          center: [-96.883923635, 30.9685011535],
+          zoom: 9
+        });
+
+        const sketch = new Sketch({
+          view: mapView,
+          layer: gLayer,     
+        });
+
+        mapView.when(() => {
+          const info = new Vue({
+            el: "#mileage",
+            components: {bbutton: "b-button"},
+            data: 
+              {
+                addButton: 'Add Road',
+                countyTotal: 120999,
+                previousTotal: 0,
+                lineLength: {},
+                editType: "New Roadbed",
+                county: "Tejas",
+                username: "DPROSACK",
+                newMiles: '',
+                result: 0,
+
+              },
+            methods:{
+              complete: function (){
+                sketch.create("polyline",{mode:"click", hasZ: false})
+                sketch.on('create', (event) => {
+                  if(event.state === "complete"){
+                    console.log("This is the previous total length: ",this.previousTotal)
+                    let lengthMiles = geometryEngine.geodesicLength(event.graphic.geometry, "miles")
+                    console.log("This is the current line length: ", parseFloat(lengthMiles.toFixed(3)));
+                    Vue.set(info.lineLength, 'length', this.previousTotal += parseFloat(lengthMiles.toFixed(3)))   
+                  }
+                });
+              }
+            },
+            watch:{
+              previousTotal: function(x,y){
+                sketch.on('update', (event) => {
+                  if(event.state === "start"){
+                    oldlengthMile = geometryEngine.geodesicLength(event.graphics[0].geometry, "miles")
+                  }
+                  if(event.state === "complete"){
+                    let newlengthMiles = geometryEngine.geodesicLength(event.graphics[0].geometry, "miles")
+                    deltaLength = oldlengthMile - newlengthMiles
+    
+                    if(newlengthMiles > oldlengthMile){
+                      Vue.set(info.lineLength, 'length', this.previousTotal += parseFloat(deltaLength.toFixed(3)))
+                    }
+                    else{
+                      Vue.set(info.lineLength, 'length', this.previousTotal -= parseFloat(deltaLength.toFixed(3)))
+                    }
+                  }
+               })
+                console.log("new: ",x, "old: ",y)
+              }
+            },
+          });
+        });
+    });
+}
